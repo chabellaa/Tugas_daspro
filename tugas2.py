d93a6c16abf519e6cd7a687eb7e448a5cd5a6bc8
Nurul zikra salzabilah <161903062+chabellaa@users.noreply.github.com>
2024-03-13 23:09:36 +0800

    Update tugas2.py

--- tugas2.py
+++ tugas2.py
@@ -3,15 +3,23 @@ data_mahasiswa = {
     'Nurul': {'password': 'pass1'},
     'Nurdalifa': {'password': 'pass2'},
     'Maulidia': {'password': 'pass3'},
+    'Manda': {'password': 'pass4'},
+    'Diva': {'password': 'pass5'},
+    'Wahdania': {'password': 'pass6'},
+    'Rahmawati': {'password': 'pass7'},
+    'Riskawati': {'password': 'pass8'},
+    'Arfa': {'password': 'pass9'},
+    'Rafika': {'password': 'pass10'},
  
 }
 
 # Fungsi login
 def login(username, password):
-    if username in data_mahasiswa and password == data_mahasiswa['Nurul']['password']:
+    if username in data_mahasiswa and password == data_mahasiswa['Nurul', 'Arfa', 'Diva', 'Manda', 'Maulidia', 'Nurdalifa', 'Rafika', 'Rahmawati',
+                                                                  'Riskawati', 'Wahdania']['password']:
         print(f"Selamat datang, {username}!")
     else:
-        print("Data yang dimasukkan salah.")
+        print("Data yang dimasukkan tidak ada.")
 
 # Input dari pengguna
 input_username = input("Masukkan username: ")
